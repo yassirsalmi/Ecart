e139474b51303635f06f6d99d4e24652f106e6fc
yassirsalmi <salmiyassir01@gmail.com>
2024-02-29 21:44:14 +0100

    updated the constant widgets in the app

---
+++ lib/constants/constant_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+Widget spaceBetweenFields() {
+  return const SizedBox(
+    height: 12,
+  );
+}
+
+Widget meduimSpace() {
+  return const SizedBox(
+    height: 20,
+  );
+}
+
+Widget bigSpace() {
+  return const SizedBox(
+    height: 30,
+  );
+}
