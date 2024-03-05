084730a37810dda8073286d7f71d627a938b95e4
yassirsalmi <salmiyassir01@gmail.com>
2024-03-05 11:05:46 +0100

    adding the constants files to the project

---
+++ lib/constants/texts.dart
@@ -0,0 +1,12 @@
+const String appName = 'Ecart';
+// auth constant
+const String logoPath = 'assets/ecart_logo.png';
+const String defaultprofilePicture = 'assets/profile_defaut.png';
+const String signInText = 'Sign In To Continue';
+const String signUpText = 'Sign Up To Continue';
+const String emailText = 'Enter your email';
+const String passwordText = 'Enter your password';
+const String nameText = 'Enter Your Name';
+const String createAccountText = 'Don\'t have an account?';
+// landing constant
+const String landingText = 'Your Style Is What We Care Most About';
