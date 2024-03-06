7e702ed185cce0b942defcd7ea655b23e413e2d8
yassirsalmi <salmiyassir01@gmail.com>
2024-03-06 15:02:34 +0100

    creating the constants files

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/widgets.dart';
 class AppColor {
   AppColor._();
   static const Color mainColor = Color(0xFF95CD2C);
+  static const Color mainColorLight = Color(0xFFCCFF90);
 }
---
+++ lib/constants/images.dart
@@ -0,0 +1,6 @@
+// import 'package:flutter/widgets.dart';
+
+class AppImages {
+  AppImages._();
+  static const appLogo = 'assets/ecart_logo.png';
+}
