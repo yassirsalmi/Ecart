6ce8bd78853602987871593274972a5c9e525791
yassirsalmi <salmiyassir01@gmail.com>
2024-03-11 18:27:48 +0000

    updated the main

--- lib/controllers/main/main_controller.dart
+++ lib/controllers/main/main_controller.dart
@@ -4,7 +4,6 @@ class MainPageController extends GetxController {
   final RxInt _selectedIndex = RxInt(0);
 
   void onItemTapped(int index) {
-    // print('Item tapped: $index');
     _selectedIndex.value = index;
   }
 
--- lib/views/main_page.dart
+++ lib/views/main_page.dart
@@ -14,42 +14,43 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Obx(
-          () => IndexedStack(
-            index: controller.selectedIndex,
-            children: const [
-              HomePage(),
-              MyCardPage(),
-              MyCardPage(),
-              ProfilePage(),
-            ],
-          ),
+      body: Obx(
+        () => IndexedStack(
+          index: controller.selectedIndex,
+          children: const [
+            HomePage(),
+            MyCardPage(),
+            MyCardPage(),
+            ProfilePage(),
+          ],
         ),
-        bottomNavigationBar: Obx(
-          () => BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: 'Search',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_bag_outlined),
-                label: 'My Card',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.account_circle),
-                label: 'Profile',
-              ),
-            ],
-            currentIndex: controller.selectedIndex,
-            selectedItemColor: AppColor.mainColor,
-            onTap: controller.onItemTapped,
-          ),
-        ));
+      ),
+      bottomNavigationBar: Obx(
+        () => BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag_outlined),
+              label: 'My Card',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: controller.selectedIndex,
+          selectedItemColor: AppColor.mainColor,
+          onTap: controller.onItemTapped,
+        ),
+      ),
+    );
   }
 }
